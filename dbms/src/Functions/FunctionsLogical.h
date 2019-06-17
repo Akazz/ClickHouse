a09293469b76df4b590ac9c9e457952da23bd1a0
akazz <akazz@95.108.173.84-red.dhcp.yndx.net>
2019-06-17 15:54:23 +0300

    WIP - Unfinished template AssociativeOperationImpl executer

--- dbms/src/Functions/FunctionsLogical.h
+++ dbms/src/Functions/FunctionsLogical.h
@@ -30,11 +30,11 @@ namespace ErrorCodes
 {
     extern const int LOGICAL_ERROR;
     extern const int ILLEGAL_TYPE_OF_ARGUMENT;
-    extern const int NUMBER_OF_ARGUMENTS_DOESNT_MATCH;
+    extern const int TOO_FEW_ARGUMENTS_FOR_FUNCTION;
     extern const int ILLEGAL_COLUMN;
 }
 
-/** Behaviour in presence of NULLs:
+/** Behaviour in the presence of NULLs:
   *
   * Functions AND, XOR, NOT use default implementation for NULLs:
   * - if one of arguments is Nullable, they return Nullable result where NULLs are returned when at least one argument was NULL.
@@ -43,65 +43,43 @@ namespace ErrorCodes
   * It always return non-Nullable result and NULL are equivalent to 0 (false).
   * For example, 1 OR NULL returns 1, not NULL.
   */
+namespace
+{
+
+enum class TriValent: UInt8 { False = 0, True = -1, Null = 1 };
+
+inline TriValent makeTriValentValue(bool value, bool is_null = false)
+{
+    return value * TriValent::True | is_null * TriValent::Null;
+}
+
+using UInt8Container = ColumnUInt8::Container;
+using UInt8ColumnPtrs = std::vector<const ColumnUInt8 *>;
+
+}
 
 struct AndImpl
 {
-    static inline constexpr bool isSaturable()
-    {
-        return true;
-    }
-
-    static inline constexpr bool isSaturatedValue(bool a)
-    {
-        return !a;
-    }
-
-    static inline constexpr bool apply(bool a, bool b)
-    {
-        return a && b;
-    }
-
-    static inline constexpr bool specialImplementationForNulls() { return false; }
+    static bool supports_ternary_logic = true;
+    static bool has_shortcut = true;
+    static TriValent shortcut_value = TriValent::False;
+    static inline constexpr TriValent apply(TriValent a, TriValent b) { return a & b; }
 };
 
 struct OrImpl
 {
-    static inline constexpr bool isSaturable()
-    {
-        return true;
-    }
-
-    static inline constexpr bool isSaturatedValue(bool a)
-    {
-        return a;
-    }
-
-    static inline constexpr bool apply(bool a, bool b)
-    {
-        return a || b;
-    }
-
-    static inline constexpr bool specialImplementationForNulls() { return true; }
+    static bool supports_ternary_logic = true;
+    static bool has_shortcut = true;
+    static TriValent shortcut_value = TriValent::True;
+    static inline constexpr TriValent apply(TriValent a, TriValent b) { return a | b; }
 };
 
 struct XorImpl
 {
-    static inline constexpr bool isSaturable()
-    {
-        return false;
-    }
-
-    static inline constexpr bool isSaturatedValue(bool)
-    {
-        return false;
-    }
-
-    static inline constexpr bool apply(bool a, bool b)
-    {
-        return a != b;
-    }
-
-    static inline constexpr bool specialImplementationForNulls() { return false; }
+    static bool supports_ternary_logic = false;
+    static bool has_shortcut = false;
+    static TriValent shortcut_value = TriValent::Null;
+    static inline constexpr TriValent apply(TriValent a, TriValent b) { return a ^ b; }
 
 #if USE_EMBEDDED_COMPILER
     static inline llvm::Value * apply(llvm::IRBuilder<> & builder, llvm::Value * a, llvm::Value * b)
@@ -130,15 +108,81 @@ struct NotImpl
 };
 
 
-using UInt8Container = ColumnUInt8::Container;
-using UInt8ColumnPtrs = std::vector<const ColumnUInt8 *>;
+std::pair<ColumnRawPtrs, ColumnRawPtrs> separateConstAndVarColumns(const ColumnRawPtrs & columns)
+{
+    ColumnRawPtrs const_columns;
+    ColumnRawPtrs var_columns;
+
+    const_columns.reserve(columns.size());
+    var_columns.reserve(columns.size());
+
+    for (const auto col : columns)
+    {
+        if (col->isColumnConst())
+        {
+            const_columns.push_back(col);
+        }
+        else
+        {
+            var_columns.push_back(col);
+        }
+    }
+
+    return {const_columns, var_columns};
+}
+
+Columns buildConvertedUInt8Columns(ColumnRawPtrs & columns)
+{
+    Columns result;
+    for (auto column : columns)
+    {
+        const size_t rows_count = column->size();
+
+        if (column->isColumnConst())
+        {
+            TriValent converted_value;
+            if (column->isColumnNullable())
+                converted_value = makeTriValentValue(column->getUInt(0), column->isNullAt(0));
+            else
+                converted_value = makeTriValentValue(column->getUInt(0));
+
+            result.push_back(
+                ColumnConst::create(ColumnUInt8::create(1, converted_value), rows_count)
+            );
+        }
+        else
+        {
+            const auto new_column = ColumnUInt8::create(rows_count);
+            auto & new_data = new_column->getData();
+
+            if (const auto nullable_column = checkAndGetColumn<ColumnNullable>(column))
+            {
+                const auto & null_map = nullable_column->getNullMapData();
+                const auto nested_column = nullable_column->getNestedColumn();
+
+                for (size_t i = 0; i < rows_count; ++i)
+                    new_data[i] = makeTriValentValue(nested_column.getUInt(i), null_map[i]);
+            }
+            else
+            {
+                for (size_t i = 0; i < rows_count; ++i)
+                    new_data[i] = makeTriValentValue(column->getUInt(i));
+            }
+
+            result.push_back(new_column);
+        }
+    }
+
+    return result;
+}
 
 
-template <typename Op, size_t N>
+template <typename Op, typename VectorType, size_t N>
 struct AssociativeOperationImpl
 {
     /// Erases the N last columns from `in` (if there are less, then all) and puts into `result` their combination.
-    static void NO_INLINE execute(UInt8ColumnPtrs & in, UInt8Container & result)
+//    static void NO_INLINE execute(std::vector<const IColumn *> & in, UInt8Container & result)
+    static void NO_INLINE execute(Columns & in, UInt8Container & result)
     {
         if (N > in.size())
         {
@@ -149,14 +193,12 @@ struct AssociativeOperationImpl
         AssociativeOperationImpl<Op, N> operation(in);
         in.erase(in.end() - N, in.end());
 
-        size_t n = result.size();
+        const size_t n = result.size();
         for (size_t i = 0; i < n; ++i)
-        {
             result[i] = operation.apply(i);
-        }
     }
 
-    const UInt8Container & vec;
+    const VectorType & vec;
     AssociativeOperationImpl<Op, N - 1> continuation;
 
     /// Remembers the last N columns from `in`.
@@ -166,20 +208,15 @@ struct AssociativeOperationImpl
     /// Returns a combination of values in the i-th row of all columns stored in the constructor.
     inline UInt8 apply(size_t i) const
     {
-        if (Op::isSaturable())
-        {
-            UInt8 a = vec[i];
-            return Op::isSaturatedValue(a) ? a : continuation.apply(i);
-        }
-        else
-        {
-            return Op::apply(vec[i], continuation.apply(i));
-        }
+        UInt8 a = vec[i];
+        return Op::is_saturable && a == Op::shortcut_value
+            ? Op::shortcut_value
+            : Op::apply(a, continuation.apply(i));
     }
 };
 
 template <typename Op>
-struct AssociativeOperationImpl<Op, 1>
+struct AssociativeOperationImpl<Op, typename VectorType, 1>
 {
     static void execute(UInt8ColumnPtrs &, UInt8Container &)
     {
@@ -198,6 +235,42 @@ struct AssociativeOperationImpl<Op, 1>
 };
 
 
+template <typename Op>
+ColumnUInt8::Ptr executeAssociativeOperation(const ColumnPtr const_column, const Columns & input_columns, size_t rows_count)
+{
+    ColumnUInt8::MutablePtr result_column;
+
+    if (const_column != nullptr)
+    {
+        result_column = const_column->convertToFullColumnIfConst();
+    }
+    else
+    {
+        result_column = ColumnUInt8::create(rows_count);
+    }
+
+    std::vector<const IColumn *> input_column_ptrs;
+    for (const auto & column : input_columns) {
+        input_column_ptrs.push_back(column.get());
+    }
+
+    if (const_column != nullptr)
+        input_column_ptrs.push_back(result_column.get());
+
+    while (input_column_ptrs.size() > 1)
+    {
+        /// TODO: Rewrite the comment below
+        /// Effeciently combine all the columns of the correct type.
+        /// With a large block size, combining 10 columns per pass is the fastest.
+        /// When small - more, is faster.
+        AssociativeOperationImpl<Op, 10>::execute(input_column_ptrs, result_column->getData());
+        input_column_ptrs.push_back(result_column.get());
+    }
+
+    return result_column;
+}
+
+
 template <typename Impl, typename Name>
 class FunctionAnyArityLogical : public IFunction
 {
@@ -206,34 +279,8 @@ public:
     static FunctionPtr create(const Context &) { return std::make_shared<FunctionAnyArityLogical>(); }
 
 private:
-    bool extractConstColumns(ColumnRawPtrs & in, UInt8 & res)
-    {
-        bool has_res = false;
-        for (int i = static_cast<int>(in.size()) - 1; i >= 0; --i)
-        {
-            if (!in[i]->isColumnConst())
-                continue;
-
-            Field value = (*in[i])[0];
-
-            UInt8 x = !value.isNull() && applyVisitor(FieldVisitorConvertToNumber<bool>(), value);
-            if (has_res)
-            {
-                res = Impl::apply(res, x);
-            }
-            else
-            {
-                res = x;
-                has_res = true;
-            }
-
-            in.erase(in.begin() + i);
-        }
-        return has_res;
-    }
-
     template <typename T>
-    bool convertTypeToUInt8(const IColumn * column, UInt8Container & res)
+    static bool convertTypeToUInt8(const IColumn * column, UInt8Container & res) const
     {
         auto col = checkAndGetColumn<ColumnVector<T>>(column);
         if (!col)
@@ -247,26 +294,27 @@ private:
     }
 
     template <typename T>
-    bool convertNullableTypeToUInt8(const IColumn * column, UInt8Container & res)
+    static bool convertNullableTypeToUInt8(const IColumn * column, UInt8Container & res) const
     {
         auto col_nullable = checkAndGetColumn<ColumnNullable>(column);
 
-        auto col = checkAndGetColumn<ColumnVector<T>>(&col_nullable->getNestedColumn());
+        auto col = checkAndGetColumn<ColumnVector<T>>(col_nullable->getNestedColumnPtr());
         if (!col)
             return false;
 
-        const auto & vec = col->getData();
         const auto & null_map = col_nullable->getNullMapData();
+        const auto & vec = col->getData();
 
-        size_t n = res.size();
+        const size_t n = res.size();
         for (size_t i = 0; i < n; ++i)
-            res[i] = !!vec[i] && !null_map[i];
+            res[i] = !null_map[i] && !!vec[i];
 
         return true;
     }
 
-    void convertToUInt8(const IColumn * column, UInt8Container & res)
+    static void convertToUInt8(const IColumn * column, UInt8Container & res) const
     {
+        convertTypeToUInt8<Int8>(column, res);
         if (!convertTypeToUInt8<Int8>(column, res) &&
             !convertTypeToUInt8<Int16>(column, res) &&
             !convertTypeToUInt8<Int32>(column, res) &&
@@ -298,7 +346,7 @@ public:
     bool isVariadic() const override { return true; }
     size_t getNumberOfArguments() const override { return 0; }
 
-    bool useDefaultImplementationForNulls() const override { return !Impl::specialImplementationForNulls(); }
+    bool useDefaultImplementationForNulls() const override { return Impl::supports_ternary_logic; }
 
     /// Get result types by argument types. If the function does not apply to these arguments, throw an exception.
     DataTypePtr getReturnTypeImpl(const DataTypes & arguments) const override
@@ -306,52 +354,62 @@ public:
         if (arguments.size() < 2)
             throw Exception("Number of arguments for function " + getName() + " doesn't match: passed "
                 + toString(arguments.size()) + ", should be at least 2.",
-                ErrorCodes::NUMBER_OF_ARGUMENTS_DOESNT_MATCH);
+                ErrorCodes::TOO_FEW_ARGUMENTS_FOR_FUNCTION);
 
+        bool has_nullable_args = false;
         for (size_t i = 0; i < arguments.size(); ++i)
+        {
+            has_nullable_args |= arguments[i]->isNullable();
             if (!(isNativeNumber(arguments[i])
-                || (Impl::specialImplementationForNulls() && (arguments[i]->onlyNull() || isNativeNumber(removeNullable(arguments[i]))))))
+                || (Impl::supports_ternary_logic && (arguments[i]->onlyNull() || isNativeNumber(removeNullable(arguments[i]))))))
                 throw Exception("Illegal type ("
                     + arguments[i]->getName()
                     + ") of " + toString(i + 1) + " argument of function " + getName(),
                     ErrorCodes::ILLEGAL_TYPE_OF_ARGUMENT);
+        }
 
-        return std::make_shared<DataTypeUInt8>();
+        DataTypePtr resultType = std::make_shared<DataTypeUInt8>();
+        return (has_nullable_args && Impl::supports_ternary_logic)
+            ? makeNullable(resultType)
+            : resultType;
     }
 
     void executeImpl(Block & block, const ColumnNumbers & arguments, size_t result, size_t /*input_rows_count*/) override
     {
-        size_t num_arguments = arguments.size();
-        ColumnRawPtrs in(num_arguments);
-        for (size_t i = 0; i < num_arguments; ++i)
-            in[i] = block.getByPosition(arguments[i]).column.get();
+        ColumnRawPtrs arg_columns(arguments.size());
+        for (size_t i = 0; i < arguments.size(); ++i)
+            arg_columns[i] = block.getByPosition(arguments[i]).column.get();
 
-        size_t rows = in[0]->size();
+        auto separated_columns = separateConstAndVarColumns(arg_columns);
 
-        /// Combine all constant columns into a single value.
-        UInt8 const_val = 0;
-        bool has_consts = extractConstColumns(in, const_val);
+        auto converted_const_columns = buildConvertedUInt8Columns(separated_columns.first);
+        bool has_consts = converted_const_columns.size() > 0;
 
-        // If this value uniquely determines the result, return it.
-        if (has_consts && (in.empty() || Impl::apply(const_val, 0) == Impl::apply(const_val, 1)))
+        ColumnUInt8::Ptr const_column_result = executeAssociativeOperation<Impl>(nullptr, converted_const_columns, 1);
+        const auto const_value = const_column_result->getElement(0);
+
+        // If const_value uniquely determines the result, return it.
+        if (has_consts && Impl::has_shortcut && const_value == Impl::shortcut_value)
         {
-            if (!in.empty())
-                const_val = Impl::apply(const_val, 0);
-            block.getByPosition(result).column = DataTypeUInt8().createColumnConst(rows, toField(const_val));
+            block.getByPosition(result).column = const_column_result;
             return;
         }
 
-        /// If this value is a neutral element, let's forget about it.
-        if (has_consts && Impl::apply(const_val, 0) == 0 && Impl::apply(const_val, 1) == 1)
+        /// If const_value equals neutral element, let's forget about it.
+        if (has_consts &&
+            Impl::apply(const_value, TriValent::False) == TriValent::False &&
+            Impl::apply(const_value, TriValent::True) == TriValent::True)
             has_consts = false;
 
+        /// Here we have to convert the remaining columns to UInt8 and apply
         auto col_res = ColumnUInt8::create();
         UInt8Container & vec_res = col_res->getData();
 
+        size_t rows = col_res->size();
         if (has_consts)
         {
-            vec_res.assign(rows, const_val);
-            in.push_back(col_res.get());
+            vec_res.assign(rows, const_value);
+            arg_columns.push_back(col_res.get());
         }
         else
         {
@@ -360,9 +418,9 @@ public:
 
         /// Convert all columns to UInt8
         UInt8ColumnPtrs uint8_in;
-        Columns converted_columns;
+        Columns converted_var_columns;
 
-        for (const IColumn * column : in)
+        for (const auto column : arg_columns)
         {
             if (auto uint8_column = checkAndGetColumn<ColumnUInt8>(column))
                 uint8_in.push_back(uint8_column);
@@ -371,24 +429,15 @@ public:
                 auto converted_column = ColumnUInt8::create(rows);
                 convertToUInt8(column, converted_column->getData());
                 uint8_in.push_back(converted_column.get());
-                converted_columns.emplace_back(std::move(converted_column));
+                converted_var_columns.emplace_back(std::move(converted_column));
             }
-        }
-
-        /// Effeciently combine all the columns of the correct type.
-        while (uint8_in.size() > 1)
-        {
-            /// With a large block size, combining 6 columns per pass is the fastest.
-            /// When small - more, is faster.
-            AssociativeOperationImpl<Impl, 10>::execute(uint8_in, vec_res);
-            uint8_in.push_back(col_res.get());
         }
 
         /// This is possible if there is exactly one non-constant among the arguments, and it is of type UInt8.
         if (uint8_in[0] != col_res.get())
             vec_res.assign(uint8_in[0]->getData());
 
-        block.getByPosition(result).column = std::move(col_res);
+        block.getByPosition(result).column = executeAssociativeOperation<Impl>(converted_var_columns, rows);
     }
 
 #if USE_EMBEDDED_COMPILER
@@ -404,7 +453,7 @@ public:
                 result = Impl::apply(b, result, nativeBoolCast(b, types[i], values[i]()));
             return b.CreateSelect(result, b.getInt8(1), b.getInt8(0));
         }
-        constexpr bool breakOnTrue = Impl::isSaturatedValue(true);
+        constexpr bool breakOnTrue = Impl::isShortcutValue(true);
         auto * next = b.GetInsertBlock();
         auto * stop = llvm::BasicBlock::Create(next->getContext(), "", next->getParent());
         b.SetInsertPoint(stop);
@@ -440,7 +489,7 @@ struct UnaryOperationImpl
 
     static void NO_INLINE vector(const ArrayA & a, ArrayC & c)
     {
-        size_t size = a.size();
+        const size_t size = a.size();
         for (size_t i = 0; i < size; ++i)
             c[i] = Op::apply(a[i]);
     }
